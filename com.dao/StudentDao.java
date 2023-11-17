729699f8c298337209acc129e78a5146ce9ade70
Thoratsahil <stsahil1717@gmail.com>
2023-11-17 12:13:45 +0530

    commit4

--- com.dao/StudentDao.java
+++ com.dao/StudentDao.java
@@ -2,6 +2,7 @@
 package com.dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import com.model.Student;
 
@@ -11,11 +12,14 @@ public class StudentDao {
 	{
 		int check=0;
 		Connection con=null;
+        PreparedStatement pst=null;
 		
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc-crud","root",null);
 			String sql="insert into student(sname,saddress,spercentage) values (?,?,?)";
+            pst= con.prepareStatement(sql);
+
 
 		} catch (ClassNotFoundException | SQLException e) {
 			// TODO Auto-generated catch block
