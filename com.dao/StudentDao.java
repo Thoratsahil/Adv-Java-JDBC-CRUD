9a5fe668bcc9b9fed63c260dd861a073d6c9d93e
Thoratsahil <stsahil1717@gmail.com>
2023-11-17 12:05:14 +0530

    commit2

---
+++ com.dao/StudentDao.java
@@ -0,0 +1,27 @@
+
+package com.dao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import com.model.Student;
+
+public class StudentDao {
+	
+	public int insertstudent(Student s)
+	{
+		int check=0;
+		Connection con=null;
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc-crud","root",null);
+			
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return check;
+	}
+
+}
